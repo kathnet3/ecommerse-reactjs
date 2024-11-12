f0663ca214b4c976ef005f7be6ecf7171a63832c
Nguyen Ngo Gia Huy <ngogiahuy238@gmail.com>
2024-11-12 15:11:45 +0700

    update prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
-export default function App() {}
+import React, { useState } from 'react';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount((prevCount) => prevCount + 1);
+    };
+
+    return (
+        <div>
+            <h1>Counter</h1>
+            <p>Current count: {count}</p>
+            <button onClick={increment}>Increase</button>
+        </div>
+    );
+};
+
+export default App;
